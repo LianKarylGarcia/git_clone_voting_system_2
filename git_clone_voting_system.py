1f174bb28ae7499ee0e464456c52429baa1aa0c2
LianKarylGarcia <liankarylg@gmail.com>
2024-10-02 22:00:33 -0400

    This code successfully sets the initial vote count to 0 and restricts voting to only the IDs specified within the brackets. It is functioning as expected.

--- git_clone_voting_system.py
+++ git_clone_voting_system.py
@@ -2,4 +2,15 @@
 nominee1 = input("Enter the name of the 1st nominee: ")
 nominee2 = input("Enter the name of the 2nd nominee: ")
 
+# initially vote count for both must be 0
+nm1_votes = 0
+nm2_votes = 0
+
+voter_id = [1,2,3,4,5,6,7,8,9,10]
+
+no_of_voter = len(voter_id)
+
+
+
+
 
